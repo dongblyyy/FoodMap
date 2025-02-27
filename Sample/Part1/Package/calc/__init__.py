03e614bcf9cb4e81b7fea2d2c22cc33a1203e80f
김동환 <dhkim@rsupport.com>
2025-02-27 23:24:51 +0900

    [Part1] 파이썬 필수문법&백엔드의 이해 - 매개변수 및 패키지 실습.

---
+++ Sample/Part1/Package/calc/__init__.py
@@ -0,0 +1,9 @@
+from calc.basic import *
+from calc.advanced import *
+
+"""
+# 제한
+__all__ = [
+    "basic"
+]
+"""
---
+++ Sample/Part1/Package/module/math_module.py
@@ -0,0 +1,12 @@
+def add(x, y):
+    return x + y
+
+def subtact(x, y):
+    return x - y
+
+def mutlply(x, y):
+    return x * y
+
+
+def division(x, y):
+    return x / y
