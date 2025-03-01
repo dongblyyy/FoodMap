6490756b112863e840cbb0b447a2fff935056614
김동환 <dongblyyy@gmail.com>
2025-03-01 22:33:26 +0900

    [Part1] 파이썬 필수문법&백엔드의 이해 - 객체지향 프로그래밍 심화(은행 시스템 구현)

---
+++ Sample/Part1/Project/BankSystem/user.py
@@ -0,0 +1,26 @@
+class BankUser:
+    def __init__(self, name, money):
+        self.__accounts = []
+        self.__name = name
+        self.__money = money
+        
+    def add_account(self, account):
+        self.__accounts.append(account)
+    
+    def get_name(self):
+        return self.__name
+        
+    def get_accounts(self):
+        for account in self.__accounts:
+            account.info()
+            
+    def add_money(self, amount):
+        self.__money += amount
+        
+    def deduct_money(self, amount):
+        self.__money -= amount
+        
+    def get_aeests(self):
+        print(f"이름: {self.__name}, 보유 현금: ${self.__money}")
+        self.get_accounts()    
+    
