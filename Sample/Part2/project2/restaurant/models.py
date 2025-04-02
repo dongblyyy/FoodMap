80d53e90a7d21c75235ac9e62d7cf0d2869d061e
김동환 <dongblyyy@gmail.com>
2025-04-02 23:14:20 +0900

    [Part2] django 웹 프레임워크 - DB 모델 생성 및 적용.

---
+++ Sample/Part2/project2/restaurant/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class AModel(models.Model):
+    # 이름
+    name = models.CharField(max_length=50)
+    # 주소
+    address = models.CharField(max_length=255)
+    # 핸드폰
+    phone = models.CharField(max_length=20)
+    # 위도
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    # 경도
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    # 평점
+    rating = models.SmallIntegerField()
+    # 생성 시간
+    created_at = models.DateTimeField(auto_now_add=True)
+    # 업데이트 시간
+    updated_at = models.DateTimeField(auto_now=True)
